b7f9ee0978f6b8bb2a8db8713ce58ce00160b84a
Soumithri Chilakamarri <soumithri93@gmail.com>
2020-08-04 15:24:52 -0700

    1.adds palindrome permutation problem 2.updates unit tests

---
+++ src/hashes/palindrome_permutation.py
@@ -0,0 +1,23 @@
+""" Given a string, determine if a permutation of the string could form 
+a palindrome.
+
+Example 1:
+
+Input: "code"
+Output: false
+Example 2:
+
+Input: "aab"
+Output: true
+Example 3:
+
+Input: "carerac"
+Output: true
+
+NOTE: Do not change the function name. If you wish to change,
+please modify unit tests accordingly
+"""
+
+
+# def has_palindrome_permutation(string):
+#     pass
---
+++ src/hashes/tests/test_palindrome_permuation.py
@@ -0,0 +1,39 @@
+import unittest
+
+try:
+    from coding_problems.src.hashes.palindrome_permutation import \
+        has_palindrome_permutation
+except ImportError as e:
+    from coding_problems.src.hashes.answers.palindrome_permutation import \
+        has_palindrome_permutation
+
+
+class Test_Palindrome_Permutation(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
